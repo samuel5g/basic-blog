d42cae98405d1362f8f0b437eadbc31e6aa92b7f
samuel5g <samuelmonteiro21@gmail.com>
2023-01-12 19:29:14 -0300

    feat(data): :card_file_box: Adds file with article data

---
+++ src/articles/articles.js
@@ -0,0 +1,28 @@
+const articles = [
+  {
+    id: 0,
+    data: '2021-06-02',
+    title: 'Agora é oficial: o Windows 11 está vindo',
+    main_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.',
+  },
+  {
+    id: 1,
+    data: '2021-06-02',
+    title: 'Tim Berners-Lee vai leiloar código-fonte da web',
+    main_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.',
+  },
+  {
+    id: 2,
+    data: '2021-06-02',
+    title: 'Tem Firefox novo no pedaço e você vai querer migrar',
+    main_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.',
+  },
+  {
+    id: 3,
+    data: '2021-06-02',
+    title: 'John McAfee, criador do antivírus McAfee, morre',
+    main_content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.',
+  },
+];
+
+export default articles;
